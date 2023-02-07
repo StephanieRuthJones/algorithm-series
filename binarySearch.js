daacb60d09d4cd4ef129c736b7a70514f5b6785c
StephanieRuthJones <stephanieruthjones@gmail.com>
2023-02-07 16:13:22 -0700

    Binary search with recursion solution complete

--- binarySearch.js
+++ binarySearch.js
@@ -20,10 +20,14 @@
 // Go back to step 2.
 
 //PROBLEM 1
-//Use binary search to find the index of a given value in a sorted array.
+//Use binary search to find an element with a given value in a sorted array.
 //Return - 1 if the value is not found.
-//Example: findIndex([1,2,3,4,5], 2) -> 1
-const findIndex = (arr, val) => {
+//INPUT/OUTPUT
+//findElement([1, 2, 3, 4, 5], 2); //Output: 1
+//findElement([1, 2, 3, 4, 5], 5); //Output: 4
+//findElement([1, 2, 3, 4, 5], 6); //Output: -1
+
+const findElement = (arr, val) => {
   //set min to 0 and max to n-1 (array length - 1)
   let min = 0;
   let max = arr.length - 1;
@@ -49,71 +53,39 @@ const findIndex = (arr, val) => {
   return middle;
 };
 
-console.log(findIndex([1, 2, 3, 4, 5], 2)); //1
-console.log(findIndex([1, 2, 3, 4, 5], 5)) //4
-console.log(findIndex([1, 2, 3, 4, 5], 6)) //-1
+console.log(findElement([1, 2, 3, 4, 5], 2)); //1
+console.log(findElement([1, 2, 3, 4, 5], 5)); //4
+console.log(findElement([1, 2, 3, 4, 5], 6)); //-1
 
-//PROBLEM 2
-//Find the maximum number in a sorted array of integers.
-//Example: findMax([1,2,3,4,5]) -> 5
-//Example: findMax([1,2,3,4,5,6,7,8,9,10]) -> 10
-const findMax = (arr) => {};
+//PROBLEM 2 - binary search + recursion
+//Use binary search with recursion to find an element's index with a given value in an array.
 
-//PROBLEM 3
-//Find the number of times a certain number appears in a sorted array of integers.
-//Example: findCount([1,1,1,1,1,1,1,1,1,1], 1) -> 10
-//Example: findCount([1,1,1,1,1,1,1,1,1,1], 2) -> 0
-const findCount = (arr, val) => {
-  //Find the middle of the array.
-  let start = 0; //This line is declaring the variable start and setting its value to 0.
-  let end = arr.length - 1; //This line is declaring the variable end and setting its value to the length of the array minus 1.
-  let middle = Math.floor((start + end) / 2); //This line is declaring the variable middle and setting its value to the middle of the array.
-  //If the value of the middle of the array is not equal to the value we are looking for and the start is less than or equal to the end, run the while loop.
-  //If the value we are looking for is less than the value of the middle of the array, set the end to the middle minus 1.
-  while (arr[middle] !== val && start <= end) {
-    //This line is declaring a while loop that will run as long as the value of the middle of the array is not equal to the value we are looking for and the start is less than or equal to the end.
-    if (val < arr[middle]) end = middle - 1;
-    //This line is declaring an if statement that will run if the value we are looking for is less than the value of the middle of the array. If this is true, the end will be set to the middle minus 1.
-    else start = middle + 1; //This line is declaring an else statement that will run if the value we are looking for is greater than the value of the middle of the array. If this is true, the start will be set to the middle plus 1.
-    middle = Math.floor((start + end) / 2); //This line is declaring the variable middle and setting its value to the middle of the array.
+const cart = ["apple", "banana", "grape", "orange", "pear", "pineapple"]; //alphabetically sorted array
+const findElementRecursively = (array, targetElement, start, end) => {
+  if (start > end) return -1; //item is not in array
+  //Find the middle index
+  let middle = Math.floor((start + end) / 2);
+  //If the middle element is the target element, return the middle element
+  if (array[middle] === targetElement) return middle;
+  //If the middle element is greater than the target element,
+  //then call the function again, but subtract 1 from the middle element
+  console.log("middle, array[middle]", middle, array[middle]);
+  console.log("targetElement", targetElement);
+  if (array[middle] > targetElement) {
+    return findElementRecursively(array, targetElement, start, middle - 1);
   }
-  //If the value of the middle of the array is not equal to the value we are looking for, return 0.
-  if (arr[middle] !== val) return 0; //This line is declaring an if statement that will run if the value of the middle of the array is not equal to the value we are looking for. If this is true, it will return 0.
-  let count = 1; //This line is declaring the variable count and setting its value to 1.
-  let left = middle - 1; //This line is declaring the variable left and setting its value to the middle minus 1.
-  let right = middle + 1; //This line is declaring the variable right and setting its value to the middle plus 1.
-  while (arr[left] === val) {
-    //This line is declaring a while loop that will run as long as the value of the left of the array is equal to the value we are looking for.
-    count++; //This line is incrementing the count by 1.
-    left--; //This line is decrementing the left by 1.
-  }
-  while (arr[right] === val) {
-    //This line is declaring a while loop that will run as long as the value of the right of the array is equal to the value we are looking for.
-    count++; //This line is incrementing the count by 1.
-    right++; //This line is incrementing the right by 1.
-  }
-  return count; //This line is declaring a return statement that will return the count.
+  return findElementRecursively(array, targetElement, middle + 1, end);
 };
 
-//PROBLEM 4
-//Find the index of a certain number in a sorted array which appears multiple times.
-const binarySearchMultiple = (arr, target) => {
-  let start = 0;
-  let end = arr.length - 1;
-  let index = -1;
+console.log(findElementRecursively(cart, "orange", 0, cart.length - 1)); //3
+console.log(findElementRecursively(cart, "pineapple", 0, cart.length - 1)); //5
+console.log(findElementRecursively(cart, "apple", 0, cart.length - 1)); //0
+console.log(findElementRecursively(cart, "kiwi", 0, cart.length - 1)); //-1
 
-  while (start <= end) {
-    let middle = Math.floor((start + end) / 2);
-
-    if (arr[middle] === target) {
-      index = middle;
-      end = middle - 1;
-    } else if (target < arr[middle]) {
-      end = middle - 1;
-    } else {
-      start = middle + 1;
-    }
-  }
-
-  return index;
-};
+console.log("this is the final console log orange > grape", "orange" > "grape");
+// EXPLANATION: In JavaScript, comparison operators use
+//"lexicographical order" to compare two strings.
+//When strings are compared, the first character of each string is looked at and compared.
+//If the first character from each string is the same, then the second character from each string is compared, and so on.
+// In this case, "orange" starts with an "o" which comes after "g" in the alphabet,
+//so "orange" is considered to be greater than "grape" and the comparison operator returns true.
