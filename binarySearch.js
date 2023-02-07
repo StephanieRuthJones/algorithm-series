6919c3f64046eefdab3a2f99ca2ea376e4efec5c
StephanieRuthJones <stephanieruthjones@gmail.com>
2023-02-07 15:37:54 -0700

    Update binary instructions

--- binarySearch.js
+++ binarySearch.js
@@ -1,16 +1,16 @@
 //BINARY SEARCH
 
 //PROBLEM 1
-//Use binary search to find the index of a given value in a sorted array.
+//Use binary search to find an element with a given value in a sorted array.
 //Return - 1 if the value is not found.
-//findIndex([1, 2, 3, 4, 5], 2); //Output: 1
-//findIndex([1, 2, 3, 4, 5], 5); //Output: 4
-//findIndex([1, 2, 3, 4, 5], 6); //Output: -1
-const findIndex = (arr, val) => {};
+//findElement([1, 2, 3, 4, 5], 2); //Output: 1
+//findElement([1, 2, 3, 4, 5], 5); //Output: 4
+//findElement([1, 2, 3, 4, 5], 6); //Output: -1
+const findElement = (arr, val) => {};
 
-console.log(findIndex([1, 2, 3, 4, 5], 2)); //1
-console.log(findIndex([1, 2, 3, 4, 5], 5)); //4
-console.log(findIndex([1, 2, 3, 4, 5], 6)); //-1
+console.log(findElement([1, 2, 3, 4, 5], 2)); //1
+console.log(findElement([1, 2, 3, 4, 5], 5)); //4
+console.log(findElement([1, 2, 3, 4, 5], 6)); //-1
 
 //PROBLEM 2
 //Find the maximum number in a sorted array of integers.
