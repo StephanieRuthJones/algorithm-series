041605332873bd18662ef216a905064d7a608824
StephanieRuthJones <stephanieruthjones@gmail.com>
2023-02-08 10:46:57 -0700

    Commented

--- binarySearch.js
+++ binarySearch.js
@@ -3,9 +3,6 @@
 //PROBLEM 1
 //Use binary search to find an element with a given value in a sorted array.
 //Return - 1 if the value is not found.
-//findElement([1, 2, 3, 4, 5], 2); //Output: 1
-//findElement([1, 2, 3, 4, 5], 5); //Output: 4
-//findElement([1, 2, 3, 4, 5], 6); //Output: -1
 const findElement = (arr, val) => {};
 
 console.log(findElement([1, 2, 3, 4, 5], 2)); //1
@@ -14,8 +11,10 @@ console.log(findElement([1, 2, 3, 4, 5], 6)); //-1
 
 //PROBLEM 2 - binary search + recursion
 //Use binary search to find the number of occurrences of a given value in a sorted array.
-//findOccurrences([1, 1, 2, 2, 2, 2, 3], 2); //Output: 4
-//findOccurrences([1, 1, 2, 2, 2, 2, 3], 3); //Output: 1
-//findOccurrences([1, 1, 2, 2, 2, 2, 3], 1); //Output: 2
 
-const findOccurrences = (arr, val) => {};
+// const findOccurrences = (arr, val) => {};
+
+// console.log(findOccurrences([1, 1, 2, 2, 2, 2, 3], 2)); //Output: 4
+// console.log(findOccurrences([1, 1, 2, 2, 2, 2, 3], 3)); //Output: 1
+// console.log(findOccurrences([1, 1, 2, 2, 2, 2, 3], 1)); //Output: 2
+// console.log(findOccurrences([1, 1, 2, 2, 2, 2, 3], 5)); //Output: 0
